3f590a3bbd104698824335aec99e254d5d50ae2c
Nicholas Gasior <nicholas@laatu.uk>
2017-05-06 02:20:39 +0100

    Add simple python script to run building devbox.

---
+++ build_devbox.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+def usage():
+  print "Usage: %s <target_host> <comment>" % (sys.argv[0])
+  sys.exit(0)
+
+if len(sys.argv) < 3:
+  usage()
+
+target_host = sys.argv[1]
+comment = sys.argv[2]
+if target_host == "localhost":
+  connection_local = ' --connection=local '
+else:
+  connection_local = ''
+
+command = 'ansible-playbook -i "'+target_host+',"'+connection_local+' ansible/play-devbox.yml -e comment="'+comment+'"'
+
+rc = os.system(command)
+if rc != 0:
+  sys.exit(1)
+
+sys.exit(0)
+
