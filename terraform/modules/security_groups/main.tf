11416994b2eb1d4badc898d8ae5f50561a8ef68f
Nicholas Gasior <nicholas@laatu.io>
2017-06-11 21:36:05 +0100

    Add security group 'build' that will be used for building AMIs.

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -25,3 +25,29 @@ resource "aws_security_group" "bastion" {
   }
 }
 
+resource "aws_security_group" "build" {
+  name = "build"
+  description = "build"
+  vpc_id = "${var.vpc_id}"
+
+  # ssh access from bastion
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.bastion.id}"]
+  }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- terraform/modules/security_groups/outputs.tf
+++ terraform/modules/security_groups/outputs.tf
@@ -1,3 +1,6 @@
 output "bastion_sg" {
   value = "${aws_security_group.bastion.id}"
 }
+output "build_sg" {
+  value = "${aws_security_group.build.id}"
+}
