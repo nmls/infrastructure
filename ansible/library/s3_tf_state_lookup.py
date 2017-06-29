c94a06432ba4dbb5529c76d913b207eaf96c3910
Nicholas Gasior <nicholas@laatu.io>
2017-06-29 21:12:50 +0100

    Make script getting Terraform lookup from S3 working.

--- ansible/library/s3_tf_state_lookup.py
+++ ansible/library/s3_tf_state_lookup.py
@@ -6,7 +6,28 @@ import json
 import boto3
 
 def lookup(args):
-  return {"data": "value from terraform here"}
+  # This code needs reviewing and error handling!
+
+  client = boto3.client("s3", region_name=args["region"])
+
+  key_obj  = client.get_object(Bucket=args["bucket"], Key=args["bucket_key"])
+  key_body = key_obj['Body'].read()
+
+  key_body = json.loads(key_body)
+
+  tf_key = args['tf_key'].split('.')
+
+  resource_type = tf_key[0]
+  resource_name = tf_key[1]
+  resource_property = tf_key[2]
+
+  res = resource_type+"."+resource_name
+
+  for mod in key_body["modules"]:
+    if res in mod["resources"].keys():
+        return {"data": mod["resources"][res]["primary"]["attributes"][resource_property]}
+
+  return {"data": {}}
 
 def main():
   args = {
