c8f4ca3968b719126ff7ad1d9e1f420f70c02948
Nicholas Gasior <nicholas@laatu.io>
2017-07-29 22:57:33 +0200

    Add gcc and autoconf to bootstrap_ansible script.

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum upgrade -y
-yum install -y epel-release
+yum install -y epel-release gcc autoconf
 yum install -y python python-pip python-devel openssl-devel
 pip install pip --upgrade
 pip install ansible
